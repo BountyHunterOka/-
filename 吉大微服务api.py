7512404bf5fe8392df20182eb6b0a5266febc844
BountyHunterOka <434291091spd@gmail.com>
2025-09-27 00:09:44 +0800

    Update 吉大微服务api.py

--- 吉大微服务api.py
+++ 吉大微服务api.py
@@ -131,6 +131,7 @@ def schedule_at_midnight(func, *args, **kwargs):
 
     def runner():
         time.sleep(wait_seconds)
+        time.sleep(1.5)
         try:
             func(*args, **kwargs)
         except Exception as e:
