c7d6455ee3a412a4552155427e8845a59986b577
BountyHunterOka <434291091spd@gmail.com>
2025-10-13 01:09:33 +0800

    Update 吉大微服务api.py

--- 吉大微服务api.py
+++ 吉大微服务api.py
@@ -175,9 +175,9 @@ def reserve(req: ReserveRequest, background_tasks: BackgroundTasks):
             # 你可以在这里把结果写入数据库或日志文件。为了简洁我们打印并返回 resp.text
             print('被预约时间: ' + fieldinfo_str)
             print("预约响应（执行时间 {}）: {}".format(datetime.now().isoformat(), resp.text))
-            for i in range(70):  
+            for i in range(120):  
                 if '请勿重复操作' in resp.text and force_stop:
-                    time.sleep(0.65)
+                    time.sleep(0.3)
                     resp = do_post_request(token=token, auth=auth, fieldinfo_str=fieldinfo_str, shopNum=req.shopNum)
                     print("预约响应（执行时间 {}）: {}".format(datetime.now().isoformat(), resp.text))
                     continue
